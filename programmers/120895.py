3ba5c72d0ff1485216e4949d83c528ede3f51672
윤용선 <65707703+yongsun-yoon@users.noreply.github.com>
2023-03-01 08:45:27 +0900

    20230301

---
+++ programmers/120895.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120895
+
+def solution(my_string, num1, num2):
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    answer = ''.join(my_string)
+    return answer
